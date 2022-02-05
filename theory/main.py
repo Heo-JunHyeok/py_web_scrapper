4c9dea5f13891d35f177e9e62b4ee748e2a85193
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-02-05 17:05:33 +0900

    이론강의 중 몰랐던 것

---
+++ theory/main.py
@@ -0,0 +1,22 @@
+# 1.7 keyboard Arguements
+    #       # positional Argument - 위치에 의존하는 인자
+    # def plus(a,b):
+    #     return a+b
+
+    # result=plus(2,4)
+    # print(result)
+
+    #       # keyboard Argument - Argument의 이름으로 쌍을 이루는 것
+    # def plus(a,b):
+    #     return a-b
+
+    # result=plus(b=30,a=1)
+    # print(result)
+
+    # def say_hello(name,age):
+    #     #string 안에 변수를 넣고 싶을 때 f"string~{변수}~"
+    #     return f"Hello {name} you are {age} years old"
+    #     #return "Hello" + name + "you are" + age + "years old"
+
+    # hello = say_hello(age="12",name="hyeok")
+    # print(hello)
