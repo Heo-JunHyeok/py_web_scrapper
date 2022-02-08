6ea39f48236b352c62402bff81b73cea8247976d
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-02-09 02:38:25 +0900

    ~#2.8 indeed

--- web_scrapper/indeed.py
+++ web_scrapper/indeed.py
@@ -1,28 +1,41 @@
-import requests # python library보다 더 강력한 기능, url 가져오기
-from bs4 import BeautifulSoup # HTML or XML에서 데이터 추출
+import requests  # python library보다 더 강력한 기능, url 가져오기
+from bs4 import BeautifulSoup  # HTML or XML에서 데이터 추출
 
-LIMIT=50
-INDEED_URL=f"https://kr.indeed.com/jobs?q=python&limit={LIMIT}"
+LIMIT = 50
+INDEED_URL = f"https://kr.indeed.com/jobs?q=python&limit={LIMIT}"
+
 
 def extract_indeed_pages():
-    result=requests.get(INDEED_URL)
-    soup=BeautifulSoup(result.text,"html.parser")
-    pagination=soup.find("div", {"class": "pagination"}) # 웹 페이지 숫자 부분 코드 추출
-    links=pagination.find_all('a')#링크 가져오기
-    pages=[]
+    result = requests.get(INDEED_URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class": "pagination"})  # 웹 페이지 숫자 부분 코드 추출
+    links = pagination.find_all('a')  # 링크 가져오기
+    pages = []
     for link in links[:-1]:
         pages.append(int(link.string))
-    max_page=pages[-1]
+    max_page = pages[-1]
     return max_page
 
+
+def extract_job(html):
+    # find: 조건에 처음으로 부합하는 것 찾기
+    title = html.find("span", title=True).text
+    company = html.find("span", {"class": "companyName"}).string
+    # location=html.find("div",{"class": "recJobLoc"})["data-rc-loc"]: class이름에 recJobLoc이 존재하는 div로 변환 후 그 안에 있는 data-rc-Loc에 접근
+    location = html.find("div", {"class": "companyLocation"}).text
+    job_id = html["data-jk"]
+    return {'title': title, 'company': company, 'location': location, 'link': f"https://www.indeed.com/viewjob?jk={job_id}&from=serp&vjs=3"}
+
+
 def extract_indeed_jobs(last_page):
-    jobs=[]
-    #for page in range(last_page):
-    result=requests.get(f"{INDEED_URL}&start={0*LIMIT}")
-    soup=BeautifulSoup(result.text,"html.parser")
-    results=soup.find_all('a',{"class": "fs-unmask"}) # find_all: 조건에 맞는 모든 것 찾기
-    for result in results:
-        title=result.find("span",title=True).text # find: 조건에 처음으로 부합하는 것 찾기
-        company=result.find("span",{"class": "companyName"}).string
-        print(title,company)
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{INDEED_URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        # find_all: 조건에 맞는 모든 것 찾기
+        results = soup.find_all('a', {"class": "fs-unmask"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
