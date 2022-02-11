3826d1d965d1ea9997cd69606bcfe0bf94ec3685
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-02-12 03:18:39 +0900

    +jobkorea

--- web_scrapper/jobkorea.py
+++ web_scrapper/jobkorea.py
@@ -1,21 +1,42 @@
-import requests # python library보다 더 강력한 기능, url 가져오기
-from bs4 import BeautifulSoup # HTML or XML에서 데이터 추출
+import requests  # python library보다 더 강력한 기능, url 가져오기
+from bs4 import BeautifulSoup  # HTML or XML에서 데이터 추출
 
-JOBKOREA_URL="https://www.jobkorea.co.kr/Search/?stext=python"
+URL = f"https://www.jobkorea.co.kr/Search/?stext=python"
 
-def extract_jobkorea_pages():
-    result=requests.get(JOBKOREA_URL)
 
-    soup=BeautifulSoup(result.text,"html.parser")
+def get_last_pages():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    last_page = soup.find(
+        "div", {"class": "tplPagination newVer short clear"}).find("span", {"class": "pgTotal"}).string  # 웹 페이지 숫자 부분 코드 추출
+    return int(last_page)
+    # jobkorea는 페이지 소스에 total page가 명시되어 있어 그것만 추출하면 된다.
 
-    pagination=soup.find("div", {"class": "tplPagination newVer wide"}) # 웹 페이지 숫자 부분 코드 추출
 
-    links=pagination.find_all('a')#링크 가져오기
-    pages=[]
+def extract_job(html):
+    title = html.find("div", {"class": "post-list-info"}).find("a")["title"]
+    company = html.find("div", {"class": "post-list-corp"}).find("a")["title"]
+    location = html.find(
+        "div", {"class": "post-list-info"}).find("span", {"class": "loc long"}).string
+    job_id = html["data-gno"]
+    return {'title': title, 'company': company, 'location': location, 'apply_link': f"https://www.jobkorea.co.kr/Recruit/GI_Read/{job_id}"}
 
-    for link in links[:-1]:
-        pages.append(int(link.string)) # link.get('page-no')
 
-    max_page=pages[-1]
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping JobKorea: Page: {page+1}")
+        result = requests.get(f"{URL}&tabType=recruit&Page_No={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find(
+            "div", {"class": "list-default"}).find_all("li", {"class": "list-post"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
-    return max_page
+
+def get_jobs():
+    last_page = get_last_pages()
+    jobs = extract_jobs(last_page)
+    return jobs
--- web_scrapper/main.py
+++ web_scrapper/main.py
@@ -1,8 +1,10 @@
 from indeed import get_jobs as get_indeed_jobs
 from so import get_jobs as get_so_jobs
 from save import save_to_file
+from jobkorea import get_jobs as get_jobkorea_jobs
 
+jobkorea_jobs = get_jobkorea_jobs()
 so_jobs = get_so_jobs()
 indeed_jobs = get_indeed_jobs()
-jobs = so_jobs+indeed_jobs
+jobs = so_jobs+indeed_jobs+jobkorea_jobs
 save_to_file(jobs)
